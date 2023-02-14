95e66b49677f098df6dc0910036c108b4a235287
Lucho250490 <u20142129559@usco.edu.co>
2023-02-14 08:42:01 -0500

    form api

---
+++ src/components/UsersCard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import "./styles/UserCard.css"
+
+const UsersCard = ({ user, deleteUser, setUpdatingUser, handleOnClickShow }) => {
+  const handleOnClickEdit = () => {
+    setUpdatingUser(user);
+    handleOnClickShow()
+  };
+
+  return (
+    <article className="userCard" id= "userBackgrund" >
+      <h3 className="userCard__name">
+        {user.first_name} {user.last_name}
+      </h3>
+
+      <hr />
+
+      <ul className="userCard__list">
+        <li className="userCard__item">
+          <span className="userCard__email">CORREO</span>
+          <div> {user.email}</div>
+          
+        </li>
+        <li className="userCard__item">
+          <span className="userCard__freeDay">CUMPLEAÑOS</span> 
+
+          <div>
+          <i className="bx bx-gift"></i> {user.birthday}
+
+          </div>
+          
+          
+        </li>
+      </ul>
+
+      <hr />
+
+      <footer className=" userCard__btn ">
+        <button className="btn" onClick={() => deleteUser(user.id)}>
+          <i className="bx bx-trash bx-tada UserCard__trash"></i>
+        </button>
+        <button className="btn" onClick={handleOnClickEdit}>
+          <i className="bx bx-pencil bx-spin UserCard__edit"></i>
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default UsersCard;
